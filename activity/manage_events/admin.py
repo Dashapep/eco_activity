2a8119a0ae41892673c2749d676f1ac6ffff6cfa
Dashapep <pds032005@gmail.com>
2021-04-08 21:03:52 +0300

    2

--- activity/manage_events/admin.py
+++ activity/manage_events/admin.py
@@ -3,5 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from .models import Events, Tag
 
-admin.site.register(Events)
-admin.site.register(Tag)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date_created']
+
+    class Meta:
+        model = Tag
+
+
+class EventsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'is_urgent', 'get_tags']
+    search_fields = ['title', 'content']
+    list_filter = ['tags', 'author']
+
+    class Meta:
+        model = Events
+
+
+admin.site.register(Events, EventsAdmin)
+admin.site.register(Tag, TagAdmin)
--- activity/manage_events/views.py
+++ activity/manage_events/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views import View
+from django.views.generic import TemplateView, FormView
+from .models import Events
+
+
+class MainView(TemplateView):
+    template_name = 'events_main.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            events = Events.objects.filter(author=request.user)
+            ctx = {'events': events}
+            return render(request, self.template_name, ctx)
+        else:
+            return render(request, self.template_name, {})
+
+
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    success_url = '/events/'
+    template_name = 'login.html'
+
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super(LoginView, self).form_valid()
+
+    def form_invalid(self, form):
+        return super(LoginView, self).form_invalid()
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect('/events/')
